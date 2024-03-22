2ae6f589a997eeaa8c574e467ae11dce4defeb58
THIRMALREDDY SUJITH KUMAR <116506529+sujith5nov@users.noreply.github.com>
2024-03-22 15:15:01 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,51 +1,49 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+
 const port = 3000;
 
 app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let blogPosts = [
-    { id: 1,
-   
-     
-    }
-];
+const blogPost=[
+    {title: 'Cricket', body:'hello'},
+    {title: 'Cricket', body:'hello'}
 
-app.set("view engine", "ejs");
+]
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", { blogPosts });
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs',{blogPost});
 });
 
 app.get("/post/:id", (req, res) => {
     const postId = parseInt(req.params.id);
-    const post = blogPosts.find((post) => post.id === postId);
+    const post = blogPost.find((post) => post.id === postId);
     const cssFilePath = `/styles/post${postId}.css`;
+    res.render('post.ejs',{blogPost,cssFilePath});
 
-    res.render("post.ejs", { post, cssFilePath });
-});
+})
 
-app.get("/compose", (req, res) => {
-    res.render("compose.ejs");
-});
+app.get('/compose', (req, res) => {
+    res.render('compose.ejs');
+})
 
 app.get("/back", (req, res) => {
-    res.render("index.ejs", { blogPosts });
+    res.render("index.ejs", { blogPost });
 });
 
 app.post("/compose", (req, res) => {
-    const { title, content, author } = req.body;
-
-    const newPostId = blogPosts.length + 1;
-
-    blogPosts.push({ id: newPostId, title, content, author });
-
+    const { title, body, author } = req.body;
+    const newPostId = blogPost.length + 1;
+    blogPost.push({id:newPostId,title, body, author});
     res.redirect("/");
-});
+})
 
-app.listen(port, () => {
+app.listen(port,() => {
     console.log(`Listening on port ${port}`);
-});
+})
